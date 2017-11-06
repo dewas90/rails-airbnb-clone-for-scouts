0971a401f944ff8265293e9245411a1d1cb1a2f0
Reginald de Wasseige <reginalddewasseige@gmail.com>
2017-11-06 17:14:07 +0100

    finished controller generation

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,16 @@
+class BookingsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def approve
+  end
+
+  def cancel
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+   validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
+   validates :password, presence: true, length: { in: 4..20 }
+   validates :password, :format => {:with => /^[([a-z]|[A-Z])0-9_-]{6,40}$/, message: "Your password must be at least 6 characters and include at least one number and one letter."}
+
 end
