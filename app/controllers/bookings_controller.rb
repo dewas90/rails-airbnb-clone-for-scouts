85b4f1180cbb72c0b4afe83bb1defb24000f30eb
Reginald de Wasseige <reginalddewasseige@gmail.com>
2017-11-06 18:30:51 +0100

    push to master booking and camp-site controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,32 @@
 class BookingsController < ApplicationController
+
+  before_action :set_bookings , only: [:show]
+
   def index
+    @bookings = Booking.all
   end
 
   def show
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    redirect_to booking_path(@booking)
   end
 
   def approve
   end
 
   def cancel
+  end
+
+  private
+
+  def set_bookings
+    @booking = Booking.find(params[:id])
+  end
+
+  def bookings_params
+    params.require(:bookings).permit(:profile_id, :host_id, :camp_site_id)
   end
 end
--- app/controllers/camp_sites_controller.rb
+++ app/controllers/camp_sites_controller.rb
@@ -1,22 +1,43 @@
 class CampSitesController < ApplicationController
+
+  before_action :set_camp_sites , only: [:show, :destroy, :edit]
+
   def index
+    @camp_sites = Camp_site.all
   end
 
   def show
   end
 
   def new
+    @camp_site = Camp_site.new
   end
 
   def create
+    @camp_site = Camp_site.new(camp_site_params)
+    redirect_to camp_site_path(@camp_site)
   end
 
   def edit
   end
 
   def update
+    @camp_site.update(camp_site_params)
+    redirect_to camp_site_path(@camp_site)
   end
 
   def destroy
+    @camp_site.destroy
+    redirect_to camp_sites_path
+  end
+
+  private
+
+  def set_camp_sites
+    @camp_site = Camp_site.find(params[:id])
+  end
+
+  def camp_sites_params
+    params.require(:camp_sites).permit(:title, :description, :capacity, :category, :address, :picture, :price)
   end
 end
