9237a903fc3d0b6e17c96c335f097fa239fbbc61
Diogo Heinen <eltigredc@gmail.com>
2017-11-08 10:44:37 +0100

    some change on the forms + debuging

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts "Start seeding"
 
 # users
-User.create(email: "test_user_1@gmail.com", password: "password_1")
+User.create(email: "test_user_1@gmail.com", password: "test_user_1@gmail.com")
 User.create(email: "test_user_2@gmail.com", password: "password_2")
 User.create(email: "test_user_3@gmail.com", password: "password_3")
 
@@ -16,7 +16,7 @@ Host.create(profile_id: 1, bank_account: "BE50 1000 1000 1000")
 # Camp_site
 CampSite.create(host_id: 1, title: "title_test_1", description: "description_test_1", capacity: 25, category: "category_test_1", address: "address_test_1", picture: "picture_test_1", price: 10)
 CampSite.create(host_id: 1, title: "title_test_2", description: "description_test_2", capacity: 25, category: "category_test_2", address: "address_test_2", picture: "picture_test_2", price: 10)
-
+CampSite.create(host_id: 1, title: "title_test_2", description: "description_test_2", capacity: 25, category: "category_test_2", address: "address_test_2", picture: "picture_test_2", price: 10)
 # Booking
 Booking.create(profile_id: 1, host_id: 1, camp_site_id:1)
 Booking.create(profile_id: 2, host_id: 1, camp_site_id:2)
