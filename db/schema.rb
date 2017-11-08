be14ab06d0e4a6b26ec5477d06a2cd5cb0b0514d
Diogo Heinen <eltigredc@gmail.com>
2017-11-08 12:40:54 +0100

    user journey + user form + sign up journey

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107140606) do
+ActiveRecord::Schema.define(version: 20171108112752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20171107140606) do
     t.string   "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "photo"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
