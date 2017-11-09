a1503ac080c243829ee9afec39908827e1f1e138
Carmen Longo <carmenlongo@hotmail.com>
2017-11-09 14:09:51 +0100

    added attachnary

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-
+//= require jquery-fileupload/basic
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
 
 $(document).ready(function(){
   $(".alert").delay(3000).slideUp();
